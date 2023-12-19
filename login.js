27bb20094744f84496877a3eb80b4f6c66d734b8
xixidududu <154297056+xixidududu@users.noreply.github.com>
2023-12-19 21:35:03 +0800

    Update login.js

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ function login() {
   // 这里进行用户名和密码的验证，你可以使用服务器端验证
   
   // 简单示例：如果用户名和密码匹配，跳转到 index.html
-  if (username === 'zixixiruirui' && password === 'zixixixi1015') {
+  if (username === '20001015' && password === '20051118') {
     window.location.href = 'mainpage.html';
   } else {
     // 如果不匹配，显示错误消息或者采取其他操作
